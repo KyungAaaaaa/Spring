f7dac73a0cffae367a13f52d3a11d7b904db682f
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2020-11-22 18:14:48 +0900

    MVC Pattern

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,13 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!");
         return "hello"; // resources:templates/hello.html로 연결되어 실행시킨다    }
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name")String name,Model model){
+        // @RequestParam GET방식으로 name파라미터를 가져와 (String) name이라는 변수로 전달
+        model.addAttribute("name",name);
+        //viewResolver 가 화면을 연결
+        return "hello-template";
     }
 }
