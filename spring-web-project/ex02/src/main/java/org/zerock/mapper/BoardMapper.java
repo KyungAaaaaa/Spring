a5152c902ec62f52fb68dd2875a88d7596b6af6d
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2021-04-14 23:12:13 +0900

    Insert & selectKey

--- spring-web-project/ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ spring-web-project/ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -9,4 +9,10 @@ public interface BoardMapper {
 
 //	@Select("select * from tbl_board where bno>0")
 	public List<BoardVO> getList();
+	
+	//등록
+	public void insert(BoardVO boardVO);
+	
+	//등록과 동시에 시퀀스값 알아오기
+	public void insertSelectKey(BoardVO boardVO);
 }
--- spring-web-project/ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ spring-web-project/ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -20,4 +21,29 @@ public class BoardMapperTests {
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
 	}
+
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로운 내용");
+		board.setWriter("경아");
+
+		mapper.insert(board);
+
+		log.info(board);
+	}
+
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 selectKey");
+		board.setContent("새로운 내용 selectKey");
+		board.setWriter("경아2");
+
+		mapper.insertSelectKey(board);
+
+		log.info(board);
+	}
+
 }
