0f4d974752ff201c5364ba2b90390e7b3a5b1269
unknown <kyungaaaaaa1223@gmail.com>
2021-05-12 16:42:07 +0900

    core - Member Domain Run & Test code

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+	// DIP À§¹Ý!
 	private final MemberRepository memberRepository = new MemoryMemberRepository();
 
 	@Override
