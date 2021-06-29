b6dcf3f9f3eca37a82872f1dae6538fc745d36b7
unknown <kyungaaaaaa1223@gmail.com>
2021-06-29 14:08:24 +0900

    core - change to Spring

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -10,23 +13,28 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+	
 	// 다형성을 활용해 인터페이스로 받음. 구체화를 바꾸려면 이 코드만 바꾸면됌!!
+	@Bean
 	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public DiscountPolicy dicountPolicy() {
 		//return new FixDiscountPolicy();
 		return new RateDiscountPolicy();
 	}
 
-	
 	// 생성자 주입
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
-
+	
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), dicountPolicy());
 	}
