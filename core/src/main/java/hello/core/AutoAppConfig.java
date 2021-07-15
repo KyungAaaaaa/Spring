209aafeac42350b9c4dbce516a73083f40786e73
unknown <kyungaaaaaa1223@gmail.com>
2021-07-15 14:46:41 +0900

    core - Scan Location

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -5,10 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type=FilterType.ANNOTATION,classes = Configuration.class))
-//@ComponentScan을 사용하면 @Configuration가 붙은 설정 정보도 빈에 등록되기 때문에 필터 설정.(실무사용x)
+@ComponentScan(
+		//탐색범위를 정하지않으면 (default:@ComponentScan이 붙은 성정정보 클래스가 속한 패키지를 범위로 검색)
+		//권장사항 : 패키지 위치를 지정하지않고, 설정 정보 클래스의 위치를 프로젝트 최상단에 두는것.
+		
+		//탐색할 패키지 지정.지정하지않을시 모~~~든 자바 코드를 다~~~검색
+		basePackages = "hello.core", 
+		//해당 클래스가 속한 패키지를 검색
+		basePackageClasses = AutoAppConfig.class,
+		//@ComponentScan을 사용하면 @Configuration가 붙은 설정 정보도 빈에 등록되기 때문에 필터 설정.(실무사용x)
+		excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+		)
 public class AutoAppConfig {
-	
-	
 
 }
--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -2,7 +2,10 @@ package hello.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+//해당 클래스는 스프링 부트 프로젝트 생성시 자동으로 만들어지는 클래스.
+//@ComponentScan가 속해있기때문에 자동으로 빈을 등록해주는것 
 @SpringBootApplication
 public class CoreApplication {
 
